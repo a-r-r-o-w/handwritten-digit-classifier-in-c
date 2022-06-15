3a73b853e7b8bc3c0713cfb58c545c03f6663572
a-r-r-o-w <avs050602@gmail.com>
2022-06-15 14:45:07 +0530

    change __PRETTY_FUNCTION__ to standard __func__ in macro

--- include/error_handler.h
+++ include/error_handler.h
@@ -1,7 +1,7 @@
 #ifndef HDC_ERROR_HANDLER_H
 #define HDC_ERROR_HANDLER_H
 
-#define error(message) handle_verbose_error(__FILE__, __PRETTY_FUNCTION__, __LINE__, message)
+#define error(message) handle_verbose_error(__FILE__, __func__, __LINE__, message)
 
 void handle_error (const char*);
 void handle_verbose_error (const char*, const char*, int, const char*);
